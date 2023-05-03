e627e31208148e4a05c15f8f36681a093f8ac6d8
Kaike <kaikeksr@outlook.com>
2023-05-03 10:29:51 -0300

    restaurando arquivos

--- fundamentosJS/poo.js
+++ fundamentosJS/poo.js
@@ -1,45 +1,45 @@
 //Poo
 
 class Person {
-  constructor(firstname, lastname, age) {
-    ;(this.firstname = firstname), (this.lastname = lastname), (this.age = age)
+    constructor(firstname, lastname, age) {
+      ;(this.firstname = firstname), (this.lastname = lastname), (this.age = age)
+    }
+  
+    //criando método que imprime o nome completo
+    getFullName() {
+      console.log(`${this.firstname} ${this.lastname}`)
+    }
+  
+    //Método estático
+    static speak() {
+      console.log("Olá mundo!")
+    }
   }
-
-  //criando método que imprime o nome completo
-  getFullName() {
-    console.log(`${this.firstname} ${this.lastname}`)
+  
+  const person = new Person("Kaike", "Rocha", "18")
+  
+  person.getFullName()
+  Person.speak()
+  
+  //Heranças
+  
+  class Animal {
+    constructor(name) {
+      this.name = name
+    }
+  
+    speak() {
+      console.log(`${this.name} made some noise`)
+    }
   }
-
-  //Método estático
-  static speak() {
-    console.log("Olá mundo!")
+  
+  class Dog extends Animal {
+    constructor(name) {
+      super(name)
+    }
   }
-}
-
-const person = new Person("Kaike", "Rocha", "18")
-
-person.getFullName()
-Person.speak()
-
-//Heranças
-
-class Animal {
-  constructor(name) {
-    this.name = name
-  }
-
-  speak() {
-    console.log(`${this.name} made some noise`)
-  }
-}
-
-class Dog extends Animal {
-  constructor(name) {
-    super(name)
-  }
-}
-
-const animal = new Animal("Simba")
-const dog = new Animal("bob")
-
-dog.speak()
+  
+  const animal = new Animal("Simba")
+  const dog = new Animal("bob")
+  
+  dog.speak()
