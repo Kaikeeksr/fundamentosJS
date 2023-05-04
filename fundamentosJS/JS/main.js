b83764ceda379fdeddcf0f946089c4a81d525668
Kaike <kaikeksr@outlook.com>
2023-05-04 10:58:17 -0300

    add more events

--- fundamentosJS/JS/main.js
+++ fundamentosJS/JS/main.js
@@ -5,7 +5,6 @@
 
 //        allItems.remove()
 //TESTE
-const items = document.querySelector(".items")
 
 //const submitButton = document.querySelectorAll("#submit-button")
 
@@ -13,14 +12,46 @@ const items = document.querySelector(".items")
 //  alert("clicaram nele")
 //}
 
-// método 1 -  clicar no botão com 1 click = itens somes, 2 clicks eles voltam a aparecer
+const items = document.querySelector(".items")
 const buttonHide = document.querySelector("#btnHide")
 const buttonShow = document.querySelector("#btnShow")
+const submitButton = document.querySelector("#submit")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const form = document.querySelector("my-form")
+const nameValue = nameInput.value
+const emailValue = emailInput.value
 
-buttonHide.addEventListener("click", () => {
+// método 1 -  clicar no botão com 1 click = itens somes, 2 clicks eles voltam a aparecer
+//Adicionando evento que muda o comportamento dos itens (aparecer e sumir)
+buttonHide.addEventListener("click", (event) => {
+  event.preventDefault() //impedir o comportamente o padrão do input 'recarregar a página'
   items.hidden = true
 })
 
-buttonShow.addEventListener("click", () => {
+buttonShow.addEventListener("click", (event) => {
+  event.preventDefault()
   items.hidden = false
+})
+
+//Logando o valor de um input no console
+nameInput.addEventListener("change", (event) => {
+  console.log(event.target.value)
+})
+
+emailInput.addEventListener("change", (event) => {
+  console.log(event.target.value)
+})
+
+//exbindo alert se os input estiverem vazio ao 'enviar dados'
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault()
+
+  if (nameValue === "" || emailValue === "") {
+    return alert("Por favor preencha os campos!")
+  }
+
+  //fazer os valores dos inputs aparecerem nos 'itens'
+  items.children[0].textContent = nameValue
+  items.children[1].textContent = emailValue
 })
